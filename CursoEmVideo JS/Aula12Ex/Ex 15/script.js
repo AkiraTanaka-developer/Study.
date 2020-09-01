25d52f3d6d4c48c305f3a6ffd1453811f1a5c7a5
Akira Tanaka <akirasoundexpress@gmail.com>
2020-09-01 20:36:58 -0300

    JS CursoEmVideo - Update

---
+++ CursoEmVideo JS/Aula12Ex/Ex 15/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+
+    var data = new Date ()
+    var ano = data.getFullYear ()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || fano.value > ano) {
+        window.alert('Erro! Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.creatElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'boy.png')
+            } else if (idade < 21){
+                // jovem
+                img.setAttribute('src', 'young-man.png')
+            } else if (idade < 50){
+                // adulto
+                img.setAttribute('src', 'man.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'old-man.png')
+            } 
+
+        }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'girl.png')
+            } else if ( idade < 21){
+                // jovem
+                img.setAttribute('src', 'young-woman.png')
+            } else if (idade < 50){
+                // adulto
+                img.setAttribute('src', 'woman.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'old-woman.png')
+            } 
+        }  
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos que o gênero é ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
--- CursoEmVideo JS/Aula12Ex/Ex.014/script.js
+++ CursoEmVideo JS/Aula12Ex/Ex.014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora =  13 //data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12){
+    // Bom dia!
+    img.src = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >=12 && hora <= 18){
+    // Boa Tarde!
+    img.src = 'tarde.png'
+    document.body.style.background = '#F5F6CE'
+} else {
+    // Boa noite!
+    img.src = 'noite.png' 
+    document.body.style.background = '#1B0A2A'   
+}
+
+}
