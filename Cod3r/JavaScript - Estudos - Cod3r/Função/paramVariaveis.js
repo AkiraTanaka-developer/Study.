3af425dbd1d2b4320afb56c2f9e37d60044792af
Akira Tanaka <akirasoundexpress@gmail.com>
2020-09-10 12:24:49 -0300

    Cod3r JS Update

--- Cod3r/JavaScript - Estudos - Cod3r/Função/paramVariaveis.js
+++ Cod3r/JavaScript - Estudos - Cod3r/Função/paramVariaveis.js
@@ -9,4 +9,5 @@ function soma (){
 console.log(soma())
 console.log(soma(1))
 console.log((1.1, 2.2, 3.3))
-console.log((1.1, 2.2, 3.3, 'teste'))
+console.log((1.1, 2.2, 3.3, "teste"))
+console.log(soma('a', 'b', 'c'))
